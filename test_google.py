27280a7dae162f99f7cd477b29f87a5b99d367ea
AxoloCrypt <axolocrypt@outlook.com>
2023-03-21 20:59:33 -0600

    Update test google to realtime (still to do)

--- test_google.py
+++ test_google.py
@@ -1,14 +1,37 @@
 import speech_recognition as sr
+from threading import Thread
+from queue import Queue
 
 recognizer = sr.Recognizer()
+recording = Queue()
+messages = Queue()
 
-with sr.Microphone() as source:
-    print("Listening....")
-    audio = recognizer.listen(source)
+messages.put(True)
 
-    try:
-        print("You said: " + recognizer.recognize_google(audio, language='es-MX'))
-    except sr.UnknownValueError:
-        print("Couldn't understand")
-    except sr.RequestError as re:
-        print(f'{re}')
+
+def record_microphone():
+    while not messages.empty():
+        with sr.Microphone(sample_rate=16000) as source:
+            print("Listening....")
+            audio = recognizer.listen(source)
+            recording.put(audio)
+
+
+def speech_recognition_google():
+    while not messages.empty():
+        audio = recording.get()
+
+        try:
+            print("You said: " + recognizer.recognize_google(audio, language='es-MX'))
+        except sr.UnknownValueError:
+            print("Couldn't understand")
+        except sr.RequestError as re:
+            print(f'{re}')
+
+
+print("Starting")
+record = Thread(target=record_microphone)
+record.start()
+
+recognize = Thread(target=speech_recognition_google)
+recognize.start()
